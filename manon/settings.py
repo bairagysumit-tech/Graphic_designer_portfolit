b39c6a034eb538fe15e4a66c710bc0b14f232a78
bairagysumit-tech <bairagysumit@gmail.com>
2025-10-03 23:18:00 +0530

    Update settings.py

--- manon/settings.py
+++ manon/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nextversionn@gmail.com'
-EMAIL_HOST_PASSWORD = 'cejcajvpxaggovwf' 
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = '' 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
